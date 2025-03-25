06c7665a8dae5ef4bc03599e3a76033c744d9200
Cody Potter <me@codypotter.com>
2025-03-24 20:31:50 -0400

    creates tos and privacy policy pages

--- htmx-rewrite/main.go
+++ htmx-rewrite/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	"loficode/templates"
+	"loficode/templates/pages/home"
+	"loficode/templates/pages/privacypolicy"
+	"loficode/templates/pages/tos"
+	"log"
 	"net/http"
 
 	"github.com/a-h/templ"
@@ -11,7 +14,9 @@ func main() {
 
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("public/assets"))))
 
-	http.Handle("/", templ.Handler(templates.HomePage()))
+	http.Handle("/", templ.Handler(home.Home()))
+	http.Handle("/tos", templ.Handler(tos.TermsOfService()))
+	http.Handle("/privacy-policy", templ.Handler(privacypolicy.PrivacyPolicy()))
 
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
