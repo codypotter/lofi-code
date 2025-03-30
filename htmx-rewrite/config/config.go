949c71fa2d43efbfb05cfb2f788d6b4572ba222c
Cody Potter <me@codypotter.com>
2025-03-30 15:05:31 -0400

    improved dx

--- htmx-rewrite/config/config.go
+++ htmx-rewrite/config/config.go
@@ -7,16 +7,11 @@ import (
 )
 
 type Config struct {
-	StorageUrl                string `json:"-" env:"STORAGE_URL"`
-	BaseUrl                   string `json:"-" env:"BASE_URL"`
-	LogLevel                  string `json:"-" env:"LOG_LEVEL"`
-	FirebaseApiKey            string `json:"-" env:"FIREBASE_API_KEY"`
-	FirebaseAuthDomain        string `json:"-" env:"FIREBASE_AUTH_DOMAIN"`
-	FirebaseProjectId         string `json:"-" env:"FIREBASE_PROJECT_ID"`
-	FirebaseStorageBucket     string `json:"-" env:"FIREBASE_STORAGE_BUCKET"`
-	FirebaseMessagingSenderId string `json:"-" env:"FIREBASE_MESSAGING_SENDER_ID"`
-	FirebaseAppId             string `json:"-" env:"FIREBASE_APP_ID"`
-	FirebaseMeasurementId     string `json:"-" env:"FIREBASE_MEASUREMENT_ID"`
+	StorageUrl       string `json:"-" env:"STORAGE_URL"`
+	BaseUrl          string `json:"-" env:"BASE_URL"`
+	LogLevel         string `json:"-" env:"LOG_LEVEL"`
+	DynamoDBRegion   string `json:"-" env:"DYNAMODB_REGION"`
+	DynamoDBEndpoint string `json:"-" env:"DYNAMODB_ENDPOINT"`
 }
 
 func (c *Config) String() string {
